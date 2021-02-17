9b5ab8272c6faa78851e08574835b513a77a1a17
Zara Colaco <ac106503@avcol.school.nz>
2021-02-18 12:16:03 +1300

    Add project files.

---
+++ AvcolStaff/Models/Staff.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AvcolStaff.Models
+{
+    public class Staff
+    {
+        public int RegistrationID {get; set; }
+        public string FirstName { get; set; }
+        public string Department { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; } 
+        [DataType(DataType.Date)]
+        public DateTime DateOfBirth { get; set; }
+
+    }
+}

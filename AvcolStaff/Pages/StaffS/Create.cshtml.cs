3041cc4b00e084f6a39457ff30bfdf432b4e23cb
Zara Colaco <ac106503@avcol.school.nz>
2021-07-22 21:23:45 +1200

    Update: Comments added to the teacher code creation section.

--- AvcolStaff/Pages/StaffS/Create.cshtml.cs
+++ AvcolStaff/Pages/StaffS/Create.cshtml.cs
@@ -47,7 +47,7 @@ namespace AvcolStaff.Pages.StaffS
             var LName = Staff.LastName;
             var TeacherCD = " ";
             Staff staff = null;
-            for (int f = 0; f < Staff.FirstName.Length; f++)//
+            for (int f = 0; f < Staff.FirstName.Length; f++)////Loop through the first name untill the end is reached
             {
                 var TCode = Staff.FirstName.Substring(f, 1).ToUpper();//First letter of the First name in uppercase
                 for (int i = 0; i < LName.Length - 1; i++)//for every pair of letters in the last name keeping checking untill a unique combination of letters is achievied 
@@ -56,32 +56,32 @@ namespace AvcolStaff.Pages.StaffS
                     staff = (from t1 in _context.Staff
                              where t1.TeacherCode == TeacherCD
                              select t1).FirstOrDefault();
-                    if (staff == null)//if its unique come out of the loop
+                    if (staff == null)//if its unique staff == null  otherwise take the next pair of letters in the last name
                     {
                         break;
                     }
 
                 }
-                if (staff == null)
+                if (staff == null)//No occurences of that teacher code in the the database if staff == null  otherwise take the next pair of letters in the first name
                 {
                     break;
                 }
             }
-            if (staff != null)
+            if (staff != null)//At this point if a uniwue combination has still no been achieved 
             {
-                for (int i = 0; i < LName.Length - 1; i++)
+                for (int i = 0; i < LName.Length - 1; i++)//Loop through the last name untill the end is reached
                 {
-                    TeacherCD = "X" + LName.Substring(i, 2).ToUpper();
+                    TeacherCD = "X" + LName.Substring(i, 2).ToUpper();//starting with X add a pair of letters from the last name untill a unique combination in formed
                     staff = (from t1 in _context.Staff
                              where t1.TeacherCode == TeacherCD
                              select t1).FirstOrDefault();
-                    if (staff == null)
+                    if (staff == null)//realistically at this point a unique code would have been formed already so the loop can break without need for further cases. 
                     {
                         break;
                     }
                 }
             }
-            Staff.TeacherCode = TeacherCD;
+            Staff.TeacherCode = TeacherCD;//sets the value of the teacher code
             if (Staff.HireDate > DateTime.Now || Staff.HireDate < CheckDate)
             {
                 ModelState.AddModelError("Custom", "Invalid Hire date");
